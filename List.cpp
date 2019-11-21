97a828cc962dcf1fa1764a06fd6d2764b1c99597
Gunnar R <gunnarrosenberg@gmail.com>
2019-11-20 18:16:33 -0800

    up

--- List.cpp
+++ List.cpp
@@ -3,3 +3,179 @@
 //
 
 #include "List.h"
+
+
+Text::Text()
+{
+
+}
+
+Text::Text(String word)
+{
+
+}
+
+Text::Text(const Text & obj)
+{
+
+}
+
+Text::~Text()
+{
+
+}
+
+void Text::print()
+{
+
+}
+
+void Text::setHighlight(int color)
+{
+
+}
+
+
+//- list node class methods
+
+
+ListNode::ListNode() //default constructor
+{
+    data = NULL;
+    next = NULL;
+    previous = NULL;
+}
+
+ListNode::ListNode(const ListNode & obj) //copy constructor
+{
+    data = obj.data;
+    next = obj.next;
+    previous = obj.previous;
+}
+
+ListNode::ListNode(Text * obj) //obj constructor
+{
+    data = obj;
+    next = NULL;
+    previous = NULL;
+}
+
+ListNode::~ListNode() //destructor
+{
+    if(data)
+    {
+        delete data;
+        data = NULL;
+    }
+    next = NULL;
+    previous = NULL;
+}
+
+ListNode *& ListNode::getNext() //next getter
+{
+    return(next);
+}
+
+void ListNode::setNext(ListNode * newNode) //next setter
+{
+    next = newNode;
+}
+
+ListNode *& ListNode::getPrevious() //previous getter
+{
+    return(previous);
+}
+
+void ListNode::setPrevious(ListNode * newNode) //previous setter
+{
+    previous = newNode;
+}
+
+Text *& ListNode::getData() //data getter
+{
+    return(data);
+}
+
+void ListNode::setData(Text *& newData) //data setter
+{
+    data = newData;
+}
+
+
+
+//----- list class methods
+
+
+
+List::List(int size) : size(size) //default constructor
+{
+    head = new ListNode*[size];
+
+    for(int i = 0; i < size; ++i)
+    {
+        head[i] = NULL;
+    }
+}
+
+List::~List() //destructor
+{
+    for(int i = 0; i < size; ++i)
+    {
+        deleteAll(head[i]);
+        head[i] = NULL;
+    }
+}
+
+void List::deleteAll(ListNode *& head) //delete all function
+{
+    if(!head) return;
+    deleteAll(head->getNext());
+    head->setNext(NULL);
+    delete head;
+}
+
+bool List::insert(int i, Text * obj) //insert function
+{
+    if(i >= size || i < 0)return false;
+    return insert(head[i], obj);
+}
+
+bool List::insert(ListNode *& head, Text * obj) //adds at end
+{
+    if(!head) //if the DLL is empty
+    {
+        head = new ListNode(obj);
+        head->setNext(NULL);
+        head->setPrevious(NULL);
+        return true;
+    }
+    else if(!head->getNext())
+    {
+        //set next to obj
+        ListNode * temp = new ListNode(obj);
+        head->setNext(temp);
+        temp->setPrevious(head);
+        return true;
+    }
+    return insert(head->getNext(), obj);
+}
+
+void List::removeAll() //remove all wrapper
+{
+    removeAllLoop(0);
+}
+
+void List::removeAllLoop(int i) //recursive remove all loop
+{
+    if(i >= size) return;
+    removeAll(head[i]);
+    removeAllLoop(++i);
+}
+
+void List::removeAll(ListNode *& head) //remove dll
+{
+    if(!head) return;
+    removeAll(head->getNext());
+    //remove(head);
+    head = NULL;
+}
--- List.h
+++ List.h
@@ -5,8 +5,63 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include "String.h"
+#include <cstring>
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+class Text {
+public:
+    Text();
+    Text(String word);
+    Text(const Text & obj);
+    ~Text();
+
+    void print();
+    void setHighlight(int color);
+
+private:
+    String word;
+    int highlight;
+};
+
+class ListNode {
+public:
+    ListNode();
+    ListNode(const ListNode & obj);
+    ListNode(Text * obj);
+    ~ListNode();
+
+    ListNode *& getNext();
+    void setNext(ListNode * newNode);
+    ListNode *& getPrevious();
+    void setPrevious(ListNode * newNode);
+    Text *& getData();
+    void setData(Text *& newData);
+
+private:
+    ListNode * next;
+    ListNode * previous;
+    Text * data;
+};
 
 class List {
+public:
+    List(int i);
+    ~List();
+
+    bool insert(int i, Text * obj);
+    void removeAll();
+    void removeAllLoop(int i);
+
+private:
+    int size;
+    ListNode ** head;
+
+    bool insert(ListNode *& head, Text * obj);
+    void deleteAll(ListNode *& head);
+    void removeAll(ListNode * & head);
 
 };
 
